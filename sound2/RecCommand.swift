20d699c74d2682be8baa777d25c6b7fc7a459c45
Yasunori Harada <hakase@viscuit.com>
2024-09-28 02:51:33 +0900

    command 化

---
+++ sound2/RecCommand.swift
@@ -0,0 +1,8 @@
+//
+//  RecCommand.swift
+//  sound2
+//
+//  Created by yasunori harada on 2024/09/28.
+//
+
+import Foundation
---
+++ sound2/TopMenu.swift
@@ -0,0 +1,8 @@
+//
+//  TopMenu.swift
+//  sound2
+//
+//  Created by yasunori harada on 2024/09/27.
+//
+
+import Foundation
